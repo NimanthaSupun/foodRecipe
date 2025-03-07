c92606f180b2e5d923b3799167c7f97b863363a0
Nimantha Supun <supunnimantha199@gmail.com>
2025-03-07 15:54:19 +0530

    rename demo function to fetchFood in useEffect for clarity

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from "react";
 
+const url = "https://api.spoonacular.com/recipes/complexSearch"
+
 const Search = () => {
   const [query, setQuery] = useState("Pizza");
 //   todo: syntax of the useEffect Hook
   useEffect(()=>{
-    const demo = ()=>{
+    const fetchFood = ()=>{
         console.log("demo function executed");
     }
-    demo()
   },[query])
   return (
     <div>
