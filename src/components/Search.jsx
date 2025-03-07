a07babf7d206ccc150636980679e05b599e5f42c
Nimantha Supun <supunnimantha199@gmail.com>
2025-03-07 15:59:49 +0530

    fetch data from API in useEffect on query change in Search component

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,13 +1,17 @@
 import React, { useEffect, useState } from "react";
 
 const url = "https://api.spoonacular.com/recipes/complexSearch"
+const API_KEY = "234b36f81ee74192a8f214293f825bca";
+
+
 
 const Search = () => {
   const [query, setQuery] = useState("Pizza");
 //   todo: syntax of the useEffect Hook
   useEffect(()=>{
     const fetchFood = ()=>{
-        console.log("demo function executed");
+       const res =  fetch(`${url}?query=${query}&apiKey=${API_KEY}`)
+       const data = res.json()
     }
   },[query])
   return (
