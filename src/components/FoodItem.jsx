501e3ef8371c7fa66df2f12b7479d26347183752
Nimantha Supun <supunnimantha199@gmail.com>
2025-03-08 04:53:01 +0530

    update FoodItem component to display food image and add a recipe button

--- src/components/FoodItem.jsx
+++ src/components/FoodItem.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 const FoodItem = ({food}) => {
   return (
     <div>
-        <img src="" alt="" srcset="" />
+        <img src={food.image} alt="" srcset="" />
          <h1 key={food.title}>{food.title}</h1>
+         <button>View Recipe</button>
     </div>
   )
 }
