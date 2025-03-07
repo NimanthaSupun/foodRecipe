b64e21274ca81d8150d17c4b3f5d1df0de5e54c1
Nimantha Supun <supunnimantha199@gmail.com>
2025-03-08 04:51:57 +0530

    add image placeholder to FoodItem component

--- src/components/FoodItem.jsx
+++ src/components/FoodItem.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 const FoodItem = ({food}) => {
   return (
     <div>
+        <img src="" alt="" srcset="" />
          <h1 key={food.title}>{food.title}</h1>
     </div>
   )
